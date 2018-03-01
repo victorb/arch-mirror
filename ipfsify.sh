a3cae3910fc2b71dea693eff07247b1f23c2d0bb
victorbjelkholm <victorbjelkholm@gmail.com>
2018-03-01 17:32:53 +0100

    Add commands for adding to IPFS/IPNS and running daemon

---
+++ ipfsify.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo "## Initializing IPFS Repository"
+
+export IPFS_PATH=$(pwd)/.ipfs
+ipfs init || true
+
+echo "## Setting IPFS config"
+ipfs config Addresses.API "/ip4/127.0.0.1/tcp/7001"
+ipfs config Addresses.Gateway "/ip4/127.0.0.1/tcp/7002"
+ipfs config Addresses.Swarm --json '["/ip4/0.0.0.0/tcp/7000"]'
+ipfs config --json Experimental.ShardingEnabled true
+ipfs config --json Experimental.FilestoreEnabled true
+ipfs config --json Datastore.NoSync true
+
+echo "## Generate IPNS key"
+ipfs key gen --type=rsa --size=2048 "arch-repository" || true
+
+echo "Adding `arch-repository/` to IPFS"
+
+# TODO should be with --nocopy but bug in the way...
+# ipfs add -r --raw-leaves --local --nocopy ./arch-repository | tee hash-list
+ipfs add -r --raw-leaves --local ./arch-repository | tee hash-list
+HASH="$(tail -n1 hash-list | cut -d ' ' -f2)"
+
+echo "Final hash is $HASH, publishing on IPNS"
+ipfs name publish --key="arch-repository" /ipfs/$HASH
---
+++ run-daemon.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+set -e
+
+export IPFS_PATH=$(pwd)/.ipfs
+
+# TODO find a way of applying this
+# ulimit -n unlimited
+
+ipfs daemon --enable-pubsub-experiment
