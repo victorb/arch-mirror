c5458ab71c69407ff5db83ab7ebd464e9f2a8d3d
victorbjelkholm <victorbjelkholm@gmail.com>
2018-08-14 14:40:47 +0200

    Add rankmirrors
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

--- find-fastest-mirror.sh
+++ find-fastest-mirror.sh
@@ -6,4 +6,4 @@ curl https://www.archlinux.org/mirrorlist/all/https/ > mirrorlist.default
 
 sed -i 's/^#Server/Server/' mirrorlist.default
 
-rankmirrors -v -n 10 mirrorlist.default > mirrorlist.fastest
+./rankmirrors.sh -v -n 10 mirrorlist.default > mirrorlist.fastest
